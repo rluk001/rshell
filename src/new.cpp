a6304b0848864a843481c0cac9ebe25dcf953b27
Ryan Luk <rluk001@ucr.edu>
2014-11-08 19:20:26 -0800

    updated new.cpp

--- src/new.cpp
+++ src/new.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <iostream>
 #include <string>
 #include <unistd.h>
@@ -10,46 +11,6 @@
 #include <pwd.h>
 #include <vector>
 using namespace std;
-void parse(char * line, char ** argv, char * delim)
-{	
-	int i = 0;
-	char *parsedWord;
-	parsedWord = strtok(line, delim);
-	while(parsedWord != NULL)
-	{
-		argv[i] = parsedWord;
-		i++;
-		parsedWord = strtok(NULL, delim);
-	}
-	cout << i << endl;
-	argv[i] = NULL;
-}
-
-void execute(char ** argv)
-{
-	int pid = fork();
-	if(pid == -1)
-	{
-		perror("Error: fork failed");
-		exit(1);
-	}
-	else if(pid > 0) 	// Parent process
-	{
-		if(wait(0) == -1)
-		{
-			perror("Error: wait has failed");
-			exit(1);
-		}
-	}
-	else if(pid == 0)	// Child process (exec)
-	{
-		if(execvp(argv[0], argv) == -1)
-		{
-			perror("Error: execvp failed");
-			exit(1);
-		}
-	}
-}
 
 void printUserInfo()
 {
@@ -66,109 +27,155 @@ void printUserInfo()
 	}
 }
 
-void commentCheck(char *  line)
+int execute(char ** argv)
+{	
+	int stat = 0;
+	int pid = fork();
+	if(pid == -1)
+	{
+		perror("Error: fork failed");
+		exit(1);
+	}
+	else if(pid > 0) 	// Parent process
+	{
+		if(wait(&stat) == -1)
+		{
+			perror("Error: wait has failed");
+			exit(1);
+		}
+	}
+	else if(pid == 0)	// Child process (exec)
+	{
+		if(execvp(argv[0], argv) == -1)
+		{
+			perror("Error: execvp failed");
+			exit(1);
+		}
+	}
+	return stat;
+}
+
+void commentCheck(string & input)
 {
 	bool commCheck = false;
 	int commIndex = 0;
-	for(int i = 0; i < strlen(line); i++)
+	for(int i = 0; i < input.size(); i++)
 	{
-		if(line[i] == '#')
+		if(input[i] == '#')
 		{
 			commIndex = i;
 			commCheck = true;
+			break;
 		}
 	}
 	if(commCheck)
 	{
-		for(int i = commIndex; i < strlen(line) - commIndex; i++)
-		{
-			line[i] = 0;
-		}
-		cout << "Comment Erased" << endl;
+		input =	input.substr(0, commIndex);
 	}
 }
+
+char ** parse(char * line, const char *delim)
+{
+	vector <char *> cArray;
+	char * del = strtok(line, delim);
+	while(del != NULL)
+	{
+		cArray.push_back(del);
+		del = strtok(NULL, delim);
+	}
+	char **argvCount = new char *[cArray.size()+1];
+	argvCount[cArray.size()] = 0;
+	
+	for(int i = 0; i < cArray.size(); i++)
+	{
+		argvCount[i] = new char[cArray.size() + 1];
+		strcpy(argvCount[i], cArray[i]);
+	}
+	return argvCount;
+}
+
+int parseArgs(char * line)
+{
+	char ** parsedSpaces = parse(line, " ");
+	int executeProgram = execute(parsedSpaces);
+	for(int i = 0; parsedSpaces[i] != NULL; i++)
+	{
+		delete parsedSpaces[i];
+	}
+	delete parsedSpaces;
+	return executeProgram;
+}
+
+void parseLogicOps(char * line)
+{
+	char * orFind = strchr(line, '|');
+	char * andFind = strchr(line, '&');
+	char ** parsedTokens = parse(line, "&|");
+	bool orTF = (orFind != NULL);
+	bool andTF = (andFind != NULL);
+	for(int i = 0; parsedTokens[i] != NULL; i++)
+	{
+		if(orTF)
+		{
+			if((parseArgs(parsedTokens[i])) == 0)
+			{
+				return;
+			}
+		}
+		else if(!orTF)
+		{
+			if((parseArgs(parsedTokens[i])) != 0)
+			{
+				return;
+			}
+		}
+		/*else if(andTF)
+		{
+			if(parseForArgs(parsedTokens[i]) == 0)
+			{
+				return;
+			}
+		}*/
+	}
+	for(int i = 0; parsedTokens[i] != NULL; i++)
+	{
+		delete parsedTokens[i];
+	}
+	delete parsedTokens;	
+}
+
+void parseCommands(char * line)
+{
+	char ** parsedSemis = parse(line, ";");
+	for(int i = 0; parsedSemis[i] != NULL; i++)
+	{
+		parseLogicOps(parsedSemis[i]);
+	}
+	for(int i = 0; parsedSemis[i] != NULL; i++)
+	{
+		delete parsedSemis[i];
+	}
+	delete parsedSemis;
+}
+
 int main()
 {
 	cout << "Initializing Shell... " << endl << endl;
-		
-	while(1)
+	string input;
+	char * line;
+	do
 	{
 		printUserInfo();	
-		char line[1024] = {0};
-		char ** argv;
-		char delimSpace[] = " ";
-		char delimORs[] = "||";
-		char delimANDs[] = "&&";
-		char delimSEMIs[] = ";";
-		vector <char **> cArray;
-		cin.getline(line, 1024);
-		commentCheck(line);	
-		argv = new char *[1024];
-		char * orFind, * semiFind, * andFind;
-		orFind = strchr(line, '|');
-		semiFind = strchr(line, ';');
-		andFind = strchr(line, '&');
-		bool orTF = false, semiTF = false, andTF = false;
-		int compOR = 0, compSEMI = 0, compAND = 0;
-		if(orFind != NULL)
+		getline(cin, input);
+		commentCheck(input);
+		if(input == "exit")
 		{
-			compOR = orFind - line + 1;
-			orTF = true;
+			exit(1);
 		}
-		cout << orTF << endl;
-		if(semiFind != NULL)
-		{
-			compSEMI = semiFind - line + 1;
-			semiTF = true;
-		}
-		cout << semiTF << endl;
-		if(andFind != NULL)
-		{
-			compAND = andFind - line + 1;
-			andTF = true;
-		}
-		cout << andTF << endl;
-		while(orFind != NULL || semiFind != NULL || andFind != NULL)
-		{
-			if(strchr(orFind+1, '|')-line+1 < compSEMI && strchr(orFind+1, '|')-line+1 < compAND && orFind)
-			{
-				parse(line, cArray.at(cArray.size()), delimORs);
-				orTF = false;
-			}
-			else if(strchr(semiFind+1, ';')-line+1 < compOR && strchr(semiFind+1, ';')-line+1 < compAND && semiFind)
-			{
-				parse(line, cArray.at(cArray.size()), delimSEMIs);
-				semiTF = false;
-			}
-			else if(strchr(andFind+1, '&')-line+1 < compOR && strchr(andFind+1, '&')-line+1 < compSEMI && andFind)
-			{
-				parse(line, cArray.at(cArray.size()), delimANDs);
-				andTF = false;
-			}
-		}
-		cout << "Passed Through the AND/OR/SEMIS" << endl;	
-		for(int i = 0; i < 2 ; i++)
-		{	
-			
-			cout << "out range" << endl;
-			parse(*cArray.at(i), cArray.at(i), delimSpace);
-		}
-		cout << "Out of range" << endl;
-		for(int j = 0; j < cArray.size(); j++)
-		{
-			if(*cArray.at(0) != NULL && strcmp(*cArray.at(0), "exit") == 0)
-			{
-				cout << "Exiting the Shell... " << endl << endl;
-				exit(1);
-			}
-		}
-		for(int i = 0; i < cArray.size(); i++)
-		{
-			execute(cArray.at(i));
-		}
-		cout << "Went through entire code" << endl;
-		delete [] argv;
-	}
-
+		line = new char[input.size()+1];
+		strcpy(line, input.c_str());
+		parseCommands(line);
+		delete line;
+	}while(input != "exit");	
 	return 0;
 }
